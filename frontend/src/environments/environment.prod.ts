a4c72da9065d7a4d70d2aef32c23a9ce46e01a2d
3234ROU <cyril.rousteau@opt.nc>
2024-10-17 10:31:17 +1100

    retrait du dossier frontend

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true, 
-  apiUrl: 'http://localhost:3001/api', // https://athlebackend.onrender.com/api en prod
-  emailjs: {
-    publicKey: 'EMj9AnAU3gidc6MMJ',
-    gmailServiceId: 'service_8wcoqok',
-    templateId: 'template_tgzc0t8'
-    }
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false, // true dans environment.prod.ts
-    apiUrl: 'http://localhost:3001/api', // https://athlebackend.onrender.com/api en prod
-    emailjs: {
-        publicKey: 'EMj9AnAU3gidc6MMJ',  // Remplace directement par les valeurs
-        gmailServiceId: 'service_8wcoqok',
-        templateId: 'template_tgzc0t8'
-    }
-  };
